3d03ad4b2c05cb7597e94f498fbee314b8f15626
Nosang Subba <nosangdev@gmail.com>
2024-05-10 19:15:52 +0530

    congigured shadcn ui for @repo/package

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui";
 
 function App() {
   return (
     <>
-        <button className="ui-text-rose-800 ui-text-4xl ui-font-semibold">Hello world</button>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <button className="ui-text-rose-800 ui-text-4xl ui-font-semibold">
+        Hello world
+      </button>
     </>
   );
 }
