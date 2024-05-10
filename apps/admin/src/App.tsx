2c6bf924069501c83b6c8353e32236e90881ee81
Nosang Subba <nosangdev@gmail.com>
2024-05-10 17:57:04 +0530

    setup tailwind config and init admin

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+        <button className="ui-text-rose-800 ui-text-4xl ui-font-semibold">Hello world</button>
+    </>
+  );
+}
+
+export default App;
---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,10 @@
+import { Config } from "tailwindcss/types/config";
+import sharedConfig from "@repo/tailwind-config";
+
+const config: Pick<Config, "prefix" | "presets" | "content"> = {
+  content: ["./src/**/*.tsx"],
+  prefix: "ui-",
+  presets: [sharedConfig],
+};
+
+export default config;
